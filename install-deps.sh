90ca81cb36a4b177ff5d220ad0fb33538055693f
Ruslan Baratov <ruslan_baratov@yahoo.com>
2015-08-11 22:17:13 +0300

    Linux: try docker

--- install-deps.sh
+++ install-deps.sh
@@ -6,10 +6,3 @@ if [ "`uname`" == "Darwin" ];
 then
   brew install python3
 fi
-
-if [ "`uname`" == "Linux" ];
-then
-  sudo apt-get -y update
-  sudo apt-get -y install cmake
-  sudo apt-get -y install python3
-fi
