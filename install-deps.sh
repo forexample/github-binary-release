b505c530e98dea5fc3f45be082f525480f44f779
Ruslan Baratov <ruslan_baratov@yahoo.com>
2015-08-11 22:02:36 +0300

    Use Multi-OS Travis feature

---
+++ install-deps.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+
+set -x
+
+if [ "`uname`" == "Darwin" ];
+then
+  brew install python3
+fi
+
+if [ "`uname`" == "Linux" ];
+then
+  sudo apt-get -y install cmake
+  sudo apt-get -y install python3
+fi
