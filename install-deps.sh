103180e9db74682b0f4d95c13399bc9c80bf485a
Ruslan Baratov <ruslan_baratov@yahoo.com>
2015-11-22 04:47:35 +0700

    Install CMake 3.0 on Linux VM

--- install-deps.sh
+++ install-deps.sh
@@ -6,3 +6,9 @@ if [ "`uname`" == "Darwin" ];
 then
   brew install python3
 fi
+
+if [ "`uname`" == "Linux" ];
+then
+  wget --no-check-certificate https://cmake.org/files/v3.4/cmake-3.4.0-Linux-x86_64.tar.gz
+  tar xf cmake-3.4.0-Linux-x86_64.tar.gz
+fi
